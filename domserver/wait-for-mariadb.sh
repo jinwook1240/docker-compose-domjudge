c4d15403ddf4573ae05dfcd251d8fe79d06ce7d0
Jung Jinwook <jinwook1240@gmail.com>
2020-01-15 10:45:03 +0900

    mariadb check

--- domserver/wait-for-mariadb.sh
+++ domserver/wait-for-mariadb.sh
@@ -7,10 +7,12 @@ host="$1"
 shift
 cmd="$@"
 
-until mysql -u root -prootpw --port 13306 --host=$host; do
-  >&2 echo "Mariadb is unavailable - sleeping.  command : mysql -u root -prootpw --port 13306 --host=$host"
+until mysql -u root -prootpw ; do
+  >&2 echo "Mariadb is unavailable - sleeping.  command : mysql -u root -prootpw"
   sleep 1
 done
 
 >&2 echo "Mariadb is up - executing command"
 exec $cmd
+
+
